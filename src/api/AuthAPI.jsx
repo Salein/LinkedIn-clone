9bc684ba18aae6ed9f30aced5abd6c3110c74658
Sallein <saleinkyl@gmail.com>
2023-05-31 22:47:31 +0300

    refactoring code

--- src/api/AuthAPI.jsx
+++ src/api/AuthAPI.jsx
@@ -3,20 +3,20 @@ import { auth } from '../firebaseConfig'
 
 export const LoginAPI = (email, password) => {
   try {
-   let res = signInWithEmailAndPassword(auth, email, password)
-   return res
+    let res = signInWithEmailAndPassword(auth, email, password)
+    return res
   }
-  catch(e) {
+  catch (e) {
     return e
   }
 }
 
 export const RegisterAPI = (email, password) => {
   try {
-   let res = createUserWithEmailAndPassword(auth, email, password)
-   return res
+    let res = createUserWithEmailAndPassword(auth, email, password)
+    return res
   }
-  catch(e) {
+  catch (e) {
     return e
   }
 }
--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,32 +1,33 @@
 import { useState } from 'react'
-import { RegisterAPI } from '../api/AuthAPI'
+import { LoginAPI } from '../api/AuthAPI'
+import LinkedinLogo from '../assets/linkedinLogo.png'
 import '../Sass/LoginComponent.scss'
 
 const LoginComponent = () => {
-    const [credentails, setCredentails] = useState({})
-    const login = () => {
-      try {
-        let res = RegisterAPI(credentails.email, credentails.password)
-    console.log(res)
-      }
-      catch(e) {
-        return e
-      }
+  const [credentails, setCredentails] = useState({})
+  const login = async () => {
+    try {
+      let res = await LoginAPI(credentails.email, credentails.password)
+      console.log(res)
     }
-    return (
-        <div className='login-wrapper'>
-            <h1>Login</h1>
-            <div className='auth-inputs'>
-                <input onChange={(event) =>
-                setCredentails({...credentails, email: event.target.value})}
-                className='common-input' placeholder='Enter your email' />
-                <input onChange={(event) =>
-                setCredentails({...credentails, password: event.target.value})}
-                className='common-input' placeholder='Enter your password' />
-            </div>
-            <button onClick={login} className="login-btn">Log in to LinkedIn</button>
-        </div>
-    );
+    catch (e) {
+      return e
+    }
+  }
+  return (
+    <div className='login-wrapper'>
+      <h1>Sign in</h1>
+      <div className='auth-inputs'>
+        <input onChange={(event) =>
+          setCredentails({ ...credentails, email: event.target.value })}
+          className='common-input' placeholder='Enter your email' />
+        <input onChange={(event) =>
+          setCredentails({ ...credentails, password: event.target.value })}
+          className='common-input' placeholder='Enter your password' />
+      </div>
+      <button onClick={login} className="login-btn">Sign in</button>
+    </div>
+  );
 };
 
-export default LoginComponent;
+export default LoginComponent
